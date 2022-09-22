8268c0a56d7701efd1728168da47c96333fd259f
Kanokpon <kanokpon26@gmail.com>
2022-09-22 23:04:12 +0700

    import mui

--- iter/pages/index.js
+++ iter/pages/index.js
@@ -1,11 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+
 
 export default function Home() {
   return (
     <div className={styles.main}>
       <button type="button" className={styles.planner}>Test</button>
+      <ButtonGroup variant="outlined" aria-label="outlined button group">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
     </div>
   )
 }
