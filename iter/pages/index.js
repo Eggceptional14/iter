116c1cdffe69dff8aa88e94fea54669f6f32d8a0
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-11-09 15:45:41 +0700

    chore: remove unused code

--- iter/pages/index.js
+++ iter/pages/index.js
@@ -7,13 +7,7 @@ import LoginPage from './auth/login';
 export default function Home() {
   return (
     <div className={styles.main}>
-      {/* <Button type="button" className={styles.button}>Test</Button> */}
       <LoginPage></LoginPage>
-      {/* <ButtonGroup variant="outlined" aria-label="outlined button group">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup> */}
     </div>
   )
 }
