5921ce03f197d0b2fd0428d1182b2a02cc87703b
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-11-09 09:33:08 +0700

    feat: create login page

---
+++ iter/pages/login.js
@@ -0,0 +1,16 @@
+import Button from '@mui/material/Button';
+import styles from '../styles/Login.module.css'
+
+
+export default function LoginPage() {
+    return (
+        <div className={styles.container1}>
+            <div>
+                Hello
+            </div>
+            <div>
+                2
+            </div>
+        </div>
+    )
+}
