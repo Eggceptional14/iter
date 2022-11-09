2be5b4d8b7dbe6a65036a5b5c8ef5a132d8b1f95
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-11-09 09:33:51 +0700

    chore: remove unused code

--- iter/pages/index.js
+++ iter/pages/index.js
@@ -1,20 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-
+import LoginPage from './login';
 
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <button type="button" className={styles.planner}>Test</button>
-      <ButtonGroup variant="outlined" aria-label="outlined button group">
+      {/* <Button type="button" className={styles.button}>Test</Button> */}
+      <LoginPage></LoginPage>
+      {/* <ButtonGroup variant="outlined" aria-label="outlined button group">
         <Button>One</Button>
         <Button>Two</Button>
         <Button>Three</Button>
-      </ButtonGroup>
+      </ButtonGroup> */}
     </div>
   )
 }
