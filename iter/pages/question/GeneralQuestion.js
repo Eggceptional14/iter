ce26a6922cea27034d17aca721c9374ad3d84530
Kanokpon <kanokpon26@gmail.com>
2023-01-16 16:32:01 +0700

    implement question page

---
+++ iter/pages/question/GeneralQuestion.js
@@ -0,0 +1,178 @@
+import React, { useState } from 'react';
+import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
+import { 
+  TextField, 
+  Button, 
+  IconButton, 
+  Stack, 
+  Typography, 
+  Autocomplete,
+  Chip,
+  Box,
+} from '@mui/material';
+import * as Yup from 'yup';
+import { ButtonUnstyled, InputUnstyled } from '@mui/base';
+import { faApple, faGoogle, faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import LoginForm from '../../src/sections/auth/LoginForm'
+import styles from '../../styles/Question.module.css'
+import { useRouter, Router } from 'next/router';
+import GeneralQuestionNavbar from '../../src/components/GeneralQuestionNavbar';
+
+const emailSchema = Yup.object().shape({
+  email: Yup.string().email().required(),
+});
+
+export default function GeneralQuestion() {
+    const router = useRouter()
+    // const classes = useStyles();
+    const [searchTerm, setSearchTerm] = useState('');
+    const options = ['Bangkok', 'Nonthaburi', 'Pathumthani'];
+    //date
+    const [value, setValue] = React.useState([null, null]);
+    //email
+    const [email, setEmail] = useState('');
+    const [emailTags, setEmailTags] = useState([]);
+    const [error, setError] = useState('');
+
+    const handleAddEmail = async () => {
+      try {
+        await emailSchema.validate({ email });
+        if(emailTags.indexOf(email) === -1) {
+          setEmailTags([...emailTags, email]);
+          setEmail('');
+          setError('');
+        }
+        else {
+          setError("Email already exist!")
+        }
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+  
+    const handleDelete = (emailToDelete) => () => {
+      setEmailTags((emailTags) => emailTags.filter((email) => email !== emailToDelete));
+      setError('');
+    };
+
+    return (
+        <>
+          <GeneralQuestionNavbar />
+          <div style={{ position:'absolute', zIndex: -1, top: 0 }}>
+            <svg width="1440" height="396" viewBox="0 0 1440 396" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_460_517)">
+            <path d="M1459 152.587L1424.25 143.569C1389.39 134.463 1319.89 116.426 1250.48 114.5C1181.08 112.574 1111.78 126.933 1042.37 129.209C972.971 131.573 903.465 121.942 833.859 109.684C764.253 97.4267 694.747 82.5422 625.141 91.1227C555.535 99.7907 486.029 131.836 416.625 139.716C347.222 147.596 277.919 131.311 208.516 126.495C139.112 121.68 69.6066 128.334 34.7526 131.748L1.53087e-06 135.075L1.19408e-05 16L34.7526 16C69.6066 16 139.112 16 208.516 16C277.919 16 347.222 16 416.626 16C486.029 16 555.535 16 625.141 16C694.747 16 764.253 16 833.859 16.0001C903.465 16.0001 972.971 16.0001 1042.37 16.0001C1111.78 16.0001 1181.08 16.0001 1250.48 16.0001C1319.89 16.0001 1389.39 16.0001 1424.25 16.0001L1459 16.0001L1459 152.587Z" fill="#56BEEC"/>
+            </g>
+            <rect width="1440" height="68" fill="#56BEEC"/>
+            <defs>
+            <clipPath id="clip0_460_517">
+            <rect width="1459" height="394" fill="white" transform="translate(1459 396) rotate(-180)"/>
+            </clipPath>
+            </defs>
+            </svg>
+          </div>
+          <Stack direction="column" spacing={2} sx={{ mt:'4em', position: 'absolute', justifyContent: 'center'  }}>
+            <Stack direction="row" spacing={1} sx={{ mt:'4em', alignItems:'center' }}>
+              <ArrowCircleDownIcon style={{ transform: 'rotate(90deg)',fontSize:'40px', color: 'var(--blue)' }}/>
+              <h1 style={{ color:'var(--orange)', fontSize:50, fontWeight:'700', letterSpacing:1.2, color: 'var(--blue)' }}>Plan your journey!</h1>
+            </Stack>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Autocomplete
+              options={options}
+              value={searchTerm}
+              onChange={(event, newValue) => {
+                setSearchTerm(newValue);
+                // onSearch(newValue);
+              }}
+              componentsProps={{
+                paper: {
+                  sx: {
+                    width: 350,
+                    margin: "auto"
+                  }
+                }
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  sx={{
+                    width: '20rem',
+                    backgroundColor: '#e0e0e0',
+                    borderRadius: '25px',
+                    border: 'none',
+                    boxShadow: 'none',
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: "50px",
+          
+                      legend: {
+                        marginLeft: "30px"
+                      }
+                    },
+                    "& .MuiAutocomplete-inputRoot": {
+                      paddingLeft: "20px !important",
+                      borderRadius: "50px"
+                    },
+                    "& .MuiInputLabel-outlined": {
+                      paddingLeft: "20px"
+                    },
+                    "& .MuiInputLabel-shrink": {
+                      marginLeft: "20px",
+                      paddingLeft: "10px",
+                      paddingRight: 0,
+                      background: 'transparent'
+                    }
+                  }}
+                  label="Search for a country, city, or region"
+                  variant="outlined"
+                />
+              )}
+            />
+            {/* {searchTerm && (
+              <IconButton onClick={handleClear}>
+                <ClearIcon />
+              </IconButton>
+            )} */}
+          </div>
+          <TextField
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            label="Enter email address"
+            variant="outlined"
+            type="email"
+            onKeyPress={(event) => {
+              if (event.key === 'Enter') {
+                handleAddEmail();
+              }
+            }}
+            FormHelperTextProps={{ style: { backgroundColor: 'transparent' }}}
+            style={{ 
+              width: '20rem', 
+              backgroundColor: '#e0e0e0', 
+              borderRadius: '25px', 
+              border: 'none', 
+              boxShadow: 'none',
+             }}
+            InputLabelProps={{
+              style: { paddingLeft: "20px" }
+            }}
+            InputProps={{
+              style: { paddingLeft: "20px", borderRadius: "50px" }
+            }}
+            error={!!error}
+            helperText={error}
+          />
+          <div>
+            {emailTags.map((email) => (
+              <Chip
+                key={email}
+                label={email}
+                onDelete={handleDelete(email)}
+                style={{margin: '0.5rem'}}
+              />
+            ))}
+          </div>
+          </Stack>
+        </>
+    )
+}
---
+++ iter/src/components/GeneralQuestionNavbar.js
@@ -0,0 +1,221 @@
+import React from 'react';
+import { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+
+const pages = ['Explore', 'Explore', 'Explore', 'Explore', 'Explore'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function ResponsiveAppBar() {
+  
+  //nav bar
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  //searchbar
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: 20,
+    backgroundColor: '#F5EBDB',
+    // '&:hover': {
+    //   backgroundColor: alpha(theme.palette.common.black, 0.10),
+    // },
+    marginRight: theme.spacing(5),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '25ch',
+      },
+    },
+  }));
+
+  return (
+    <AppBar position="static" sx={{ background:'var(--blue)', height: '2rem', boxShadow:'none', mt: 3, padding: '0 5% 0 5%' }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h3"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: '30rem',
+              display: { xs: 'none', md: 'flex' },
+              fontWeight: 900,
+              letterSpacing: '.3rem',
+              color: 'var(--ivory-white)',
+              '&:hover': {
+                color: 'var(--ivory-white)',
+              },
+              textDecoration: 'none',
+            }}
+          >
+            ITER
+          </Typography>
+          {/* <Search sx={{display: { xs: 'none', md: 'flex' }}}>
+            <SearchIconWrapper>
+              <SearchIcon style={{color: 'black'}}/>
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search for your destination !"
+              inputProps={{ 'aria-label': 'search' }}
+              style={{color: 'black'}}
+            />
+          </Search> */}
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              // color='inherit'
+              style={{ color: 'var(--black)'}}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontWeight: 900,
+              letterSpacing: '.3rem',
+              color: 'var(--black)',
+              textDecoration: 'none',
+            }}
+          >
+            ITER
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'var(--ivory-white)', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
